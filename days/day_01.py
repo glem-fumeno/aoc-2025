4e46e88c5340455f708a5ef5e6d9c72f010bab95
Emilia Glibowska <emilia.glibowska@elmark.com.pl>
2025-12-01 07:01:26 +0100

    solved day 01

--- days/day_01.py
+++ days/day_01.py
@@ -1,8 +1,33 @@
 def solve_part_one(input_file: str):
-    del input_file
-    return 0
+    count = 0
+    dial = 50
+    for line in input_file.splitlines():
+        direction, *magnitude = line
+        magnitude = int("".join(magnitude))
+        match direction:
+            case "R":
+                dial += magnitude
+            case "L":
+                dial -= magnitude
+        dial %= 100
+        if dial == 0:
+            count += 1
+    return count
 
 
 def solve_part_two(input_file: str):
-    del input_file
-    return 0
+    count = 0
+    dial = 50
+    for line in input_file.splitlines():
+        direction, *magnitude = line
+        magnitude = int("".join(magnitude))
+        for _ in range(magnitude):
+            match direction:
+                case "R":
+                    dial += 1
+                case "L":
+                    dial -= 1
+            dial %= 100
+            if dial == 0:
+                count += 1
+    return count
